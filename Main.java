5f6ad76a2b2168edac0146addd3cb8415621e723
Dwayne-T <dwaynetaylor763@gmail.com>
2021-09-10 21:01:50 -0600

    Hmmmm

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
-  public static void main(String args[]){
+  public static void main(String[] arg){
     System.out.print("Hello World");
+    String string = new String(new char[10]).replace('\0', '*');
+    System.out.println(string);
   }
 }
