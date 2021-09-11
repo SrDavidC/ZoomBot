f15f6bbbfc0e4aa48501b9338c0151d5395e9385
Whyvna <57416404+Whyvna@users.noreply.github.com>
2021-09-10 21:08:14 -0600

    a

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main{
     System.out.print("Hello World");
     String string = new String(new char[10]).replace('\0', '*');
     System.out.println(string);
+    dhgosdhgkfsdhzgjszhgs
   }
 }
